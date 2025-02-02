322753089ec0ce28607ef99dc9f92a791ea44d24
Suryaa <suryaacoolness@gmail.com>
2025-02-02 16:18:07 -0500

    Update pi.h

--- pi.h
+++ pi.h
@@ -10,37 +10,34 @@
 
 double pi_calc(long int n) {
     
-    // MPI Initialization
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Work Distribution
-    long int points_per_proc = n / size;
-    long int remainder = n % size;
-    long int local_points = points_per_proc + (rank < remainder ? 1 : 0);
+    long int ppc = n / size;
+    long int rem = n % size;
+    long int lp = ppc;
+    if (rank < rem) {
+        lp += 1;
+    }
 
-    // Random Seed Initialization
     srand(time(NULL) + rank);
 
-    // Monte Carlo Simulation
-    long int local_count = 0;
-    for (long int i = 0; i < local_points; i++) {
+    long int local = 0;
+    for (long int i = 0; i < lp; i++) {
         double x = rand() / (double)RAND_MAX;
         double y = rand() / (double)RAND_MAX;
         if ((x * x + y * y) <= 1.0) {
-            local_count++;
+            local++;
         }
     }
 
-    // Reduce Local Counts to Get Global Count
-    long int global_count = 0;
-    MPI_Reduce(&local_count, &global_count, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    long int global = 0;
+    MPI_Reduce(&local, &global, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    // Compute π Estimation
     double pi_estimate = 0.0;
     if (rank == 0) {
-        pi_estimate = 4.0 * ((double)global_count / (double)n);
+        pi_estimate = 4.0 * ((double)global / (double)n);
     }
 
     return pi_estimate;
